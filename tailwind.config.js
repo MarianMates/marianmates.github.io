f3885f2b2da8748dfa817527e764d5a8ef5cb28d
Marian Mates <marian_mates@carrefour.com>
2024-04-08 09:36:55 +0300

    Added styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html'],
+  content: [],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
